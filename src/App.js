62cffed279a6fab44e41a80c1f5d6b37f93382bb
Oscar Santos <oscar303data@gmail.com>
2023-11-18 10:43:56 -0700

    setting up close restaurants from my location

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     const [places, setPlaces] = useState([])
 
     const [coordinates, setCoordinates] = useState({})
-    const [bounds, setBounds] = useState(null)
+    const [bounds, setBounds] = useState({})
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
@@ -23,10 +23,10 @@ const App = () => {
     }, []);
 
     useEffect(() => {
-        console.log(coordinates, bounds)
-        getPlacesData()
+        
+        getPlacesData(bounds.sw, bounds.ne)
             .then((data) => {
-                console.log(data);
+                // console.log(data);
                 setPlaces(data)
             })
     }, [coordinates, bounds])
@@ -37,7 +37,7 @@ const App = () => {
             <Header />
             <Grid container spacing={3} style={{ width: '100%' }}>
                 <Grid item xs={12} md={4}>
-                    <List />
+                    <List places={places}/>
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Map 
--- src/api/index.js
+++ src/api/index.js
@@ -3,29 +3,31 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-const options = {
+// const options = {
 
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-  },
-  headers: {
-    'X-RapidAPI-Key': '301dc842ddmshb32b37149e9054ap1b196cjsn95cef2dbc1c7',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
-export const getPlacesData = async () => {
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
+export const getPlacesData = async (sw, ne) => {
     try {
-        const { data: { data }} = await axios.get(URL, options)
+        const { data: { data }} = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '301dc842ddmshb32b37149e9054ap1b196cjsn95cef2dbc1c7',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        })
 
         return data
     } catch (error) {
