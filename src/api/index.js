fbf2c55623e7723326769f5261a0162800085582
Oscar Santos <oscar303data@gmail.com>
2023-11-19 12:00:20 -0700

    set up template literal on the rapid api to make it dinamic and be able to load restaurants or hotels or attractions

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
 // const options = {
 
@@ -14,9 +14,10 @@ const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 // } catch (error) {
 // 	console.error(error);
 // }
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
     try {
-        const { data: { data }} = await axios.get(URL, {
+        const { data: { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+        // const { data: { data }} = await axios.get(URL, {
           params: {
             bl_latitude: sw.lat,
             tr_latitude: ne.lat,
